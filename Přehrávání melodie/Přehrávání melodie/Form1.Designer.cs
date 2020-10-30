743d75c733c1117b54dd9d5a7c895a56e826f079
Ponny159 <32883433+Ponny159@users.noreply.github.com>
2020-10-30 14:39:18 +0100

    Add files via upload

---
+++ Přehrávání melodie/Přehrávání melodie/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Přehrávání_melodie
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_play = new System.Windows.Forms.Button();
+            this.btn_filedialog = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_play
+            // 
+            this.btn_play.Location = new System.Drawing.Point(12, 151);
+            this.btn_play.Name = "btn_play";
+            this.btn_play.Size = new System.Drawing.Size(208, 51);
+            this.btn_play.TabIndex = 1;
+            this.btn_play.Text = "Přehraj";
+            this.btn_play.UseVisualStyleBackColor = true;
+            this.btn_play.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_filedialog
+            // 
+            this.btn_filedialog.Location = new System.Drawing.Point(12, 55);
+            this.btn_filedialog.Name = "btn_filedialog";
+            this.btn_filedialog.Size = new System.Drawing.Size(208, 51);
+            this.btn_filedialog.TabIndex = 0;
+            this.btn_filedialog.Text = "Najít soubor";
+            this.btn_filedialog.UseVisualStyleBackColor = true;
+            this.btn_filedialog.Click += new System.EventHandler(this.btn_filedialog_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(269, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(208, 51);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Přehraj a čekej";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(269, 151);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(208, 51);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Opakovaně přehrávej";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(143, 239);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(208, 51);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Zastav přehrávání";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(465, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "Cesta k souboru";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(488, 310);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_filedialog);
+            this.Controls.Add(this.btn_play);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_play;
+        private System.Windows.Forms.Button btn_filedialog;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Přehrávání melodie/Přehrávání melodie/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace Přehrávání_melodie
+{
+    public partial class Form1 : Form
+    {        
+        string location = null;
+        SoundPlayer player = new SoundPlayer();        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (location != null)
+            {
+                player.SoundLocation = location;
+                player.Play();               
+                MessageBox.Show("Přehrává se melodie " + player.SoundLocation);
+            }
+            else
+                MessageBox.Show("Vyberte soubor!");
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (location != null)
+            {
+                player.SoundLocation = location;
+                player.PlaySync();
+                MessageBox.Show("Přehrála se melodie " + player.SoundLocation);
+            }
+            else
+                MessageBox.Show("Vyberte soubor!");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (location != null)
+            {
+                player.SoundLocation = location;
+                player.PlayLooping();
+                MessageBox.Show("Přehrává se melodie " + player.SoundLocation);
+            }   
+            else
+                MessageBox.Show("Vyberte soubor!");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            player.Stop();
+            MessageBox.Show("Přehrávání zastaveno");
+        }
+
+        private void btn_filedialog_Click(object sender, EventArgs e)
+        {
+            //pro vyhledání souboru použiju openfiledialog
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                //filtr všech video a audio formátů pro jednodušší vyhledávání
+                ofd.Filter = "All Media Files|*.wav;*.aac;*.wma;*.wmv;*.avi;*.mpg;*.mpeg;*.m1v;*.mp2;*.mp3;*.mpa;*.mpe;*.m3u;*.mp4;*.mov;*.3g2;*.3gp2;*.3gp;*.3gpp;*.m4a;*.cda;*.aif;*.aifc;*.aiff;*.mid;*.midi;*.rmi;*.mkv;*.WAV;*.AAC;*.WMA;*.WMV;*.AVI;*.MPG;*.MPEG;*.M1V;*.MP2;*.MP3;*.MPA;*.MPE;*.M3U;*.MP4;*.MOV;*.3G2;*.3GP2;*.3GP;*.3GPP;*.M4A;*.CDA;*.AIF;*.AIFC;*.AIFF;*.MID;*.MIDI;*.RMI;*.MKV";                
+                //otevře se dialog
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    location = ofd.FileName;
+                    textBox1.Text = ofd.FileName;
+                }
+            }
+        }
+    }
+}
